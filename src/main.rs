50445719adf86339cb0c48be8eee04d830c639f2
jackyliu16 <18922251299@163.com>
2024-10-31 03:21:02 +0800

    feat: just panic

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("No enough argument");
+        };
         Self { query: args[1].clone(), file_path: args[2].clone() }
     }
 }
