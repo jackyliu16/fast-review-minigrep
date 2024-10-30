46e4bb10d629c0e37a6baddc1f799c009718c316
jackyliu16 <18922251299@163.com>
2024-10-31 03:02:37 +0800

    feat: impl basic method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main() {
     println!("Hello, world!");
 
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let file_path = &args[1]; // 0 is this file name
+
+    let contents = fs::read_to_string(&file_path).expect("Should have been able to read the file");
+
+    println!("with text:\n{contents}");
 }
