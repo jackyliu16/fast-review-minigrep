8c35e30feb12318adff89d7c085295b474da9873
jackyliu16 <18922251299@163.com>
2024-10-31 03:30:05 +0800

    feat: handler error of Config::new

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     println!("Hello, world!");
@@ -7,7 +8,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Read String From {}", config.file_path);
     let contents = fs::read_to_string(config.file_path)
@@ -21,11 +25,11 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("No enough argument");
+            return Err("No enough arguments");
         };
-        Self { query: args[1].clone(), file_path: args[2].clone() }
+        Ok(Self { query: args[1].clone(), file_path: args[2].clone() })
     }
 }
 
